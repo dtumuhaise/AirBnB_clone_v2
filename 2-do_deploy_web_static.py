d16f64902d3118d3fa68a4c7ffb86a756ad2f030
Davis Tumuhaise <tumuhaise@gmail.com>
2023-01-27 13:15:18 +0300

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,27 +13,29 @@ def do_deploy(archive_path):
 
     if not os.path.exists(archive_path):
         return False
-    archive_name = os.path.basename(archive_path)
-    archive_folder = archive_name.split(".")[0]
+    try:
+        archive_name = os.path.basename(archive_path)
+        archive_folder = archive_name.split(".")[0]
 
-    put(archive_path, '/tmp/' + archive_name)
+        put(archive_path, '/tmp/')
+        run('mkdir -p /data/web_static/releases/{}'.format(archive_folder))
 
-    run('mkdir -p /data/web_static/releases/{}'.format(archive_name))
+        run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'
+            .format(archive_name, archive_name))
 
-    run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'
-        .format(archive_name, archive_name))
+        run('rm /tmp/{}.tgz'.format(archive_name))
 
-    run('rm /tmp/{}.tgz'.format(archive_name))
+        run(('mv /data/web_static/releases/{}/web_static/* ' +
+            '/data/web_static/releases/{}/')
+            .format(archive_name, archive_name))
 
-    run(('mv /data/web_static/releases/{}/web_static/* ' +
-        '/data/web_static/releases/{}/')
-        .format(archive_name, archive_name))
+        run('rm -rf /data/web_static/current')
 
-    run('rm -rf /data/web_static/current')
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(archive_name))
 
-    run('rm -rf /data/web_static/releases/{}/web_static'
-        .format(archive_name))
-
-    run('ln -s /data/web_static/releases/{}/' +
-        ' /data/web_static/current'.format(archive_name))
-    return True
+        run('ln -s /data/web_static/releases/{}/' +
+            ' /data/web_static/current'.format(archive_name))
+        return True
+    except Exception:
+        return False
