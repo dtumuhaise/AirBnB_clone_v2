c2c0a9f725a749238224d3040dba7139ba51dfc1
Davis Tumuhaise <tumuhaise@gmail.com>
2023-01-27 09:02:36 +0300

    fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,5 +14,5 @@ sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '$ a location /hbnb_static/ { \n alias /data/web_static/current/; \n }' /etc/nginx/sites-available/default
+sudo sed -i '37i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
