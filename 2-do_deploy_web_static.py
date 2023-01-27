4d78930539591d71d895ef603a0cfd2ea9ded4da
Davis Tumuhaise <tumuhaise@gmail.com>
2023-01-27 12:59:44 +0300

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,12 +15,25 @@ def do_deploy(archive_path):
         return False
     archive_name = os.path.basename(archive_path)
     archive_folder = archive_name.split(".")[0]
+
     put(archive_path, '/tmp/' + archive_name)
-    run('mkdir -p /data/web_static/releases/' + archive_folder)
-    run('tar -xzf /tmp/' + archive_name + ' -C /data/web_static\
-        /releases/' + archive_folder)
-    run('rm /tmp/' + archive_name)
+
+    run('mkdir -p /data/web_static/releases/{}'.format(archive_name))
+
+    run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'
+        .format(archive_name, archive_name))
+
+    run('rm /tmp/{}.tgz'.format(archive_name))
+
+    run(('mv /data/web_static/releases/{}/web_static/* ' +
+        '/data/web_static/releases/{}/')
+        .format(archive_name, archive_name))
+
     run('rm -rf /data/web_static/current')
-    run('ln -s /data/web_static/releases/' + archive_folder +
-        ' /data/web_static/current')
+
+    run('rm -rf /data/web_static/releases/{}/web_static'
+        .format(archive_name))
+
+    run('ln -s /data/web_static/releases/{}/' +
+        ' /data/web_static/current'.format(archive_name))
     return True
